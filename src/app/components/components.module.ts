40b28da6c34ed3862a798c250e45609a048b8dde
kdev <kdev.software1110@gmail.com>
2019-04-29 12:23:10 -0400

    import dropdown module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhonepickerComponent } from './phonepicker/phonepicker.component';
 
+import { DropdownModule } from 'primeng/dropdown';
+
 @NgModule({
   declarations: [PhonepickerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    DropdownModule
   ],
   exports: [
     PhonepickerComponent
