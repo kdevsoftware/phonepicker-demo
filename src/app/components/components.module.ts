37924463c795302c005dff1bf9d5568efc3e4705
kdev <kdev.software1110@gmail.com>
2019-04-29 12:09:07 -0400

    create components module and phonepicker component

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhonepickerComponent } from './phonepicker/phonepicker.component';
+
+@NgModule({
+  declarations: [PhonepickerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/components/phonepicker/phonepicker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phonepicker',
+  templateUrl: './phonepicker.component.html',
+  styleUrls: ['./phonepicker.component.scss']
+})
+export class PhonepickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
