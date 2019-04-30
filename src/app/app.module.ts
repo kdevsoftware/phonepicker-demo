759b6eca9e6b6282095402712ee352f2480106a1
kdev <kdev.software1110@gmail.com>
2019-04-30 02:50:38 -0400

    refactoring app module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { ComponentsModule } from './components/components.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +12,7 @@ import { ComponentsModule } from './components/components.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
+
     ComponentsModule
   ],
   providers: [],
