257b538c5d57a22dd876471b3c8c222a3a042abe
kdev <kdev.software1110@gmail.com>
2019-04-30 02:53:41 -0400

    refactoring component module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PhonepickerComponent } from './phonepicker/phonepicker.component';
-
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+
 import { DropdownModule } from 'primeng/dropdown';
 import { KeyFilterModule } from 'primeng/keyfilter';
 import { ButtonModule } from 'primeng/button';
-import { HttpClientModule } from '@angular/common/http';
+
 import { PhonepickerService } from './phonepicker/phonepicker.service';
+
+import { PhonepickerComponent } from './phonepicker/phonepicker.component';
 
 @NgModule({
   declarations: [PhonepickerComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     FormsModule,
     DropdownModule,
     KeyFilterModule,
-    ButtonModule,
-    HttpClientModule
+    ButtonModule
   ],
   exports: [
     PhonepickerComponent
