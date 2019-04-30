93bfd24c729b01770ca1987470c4fcf491fc38d7
kdev <kdev.software1110@gmail.com>
2019-04-30 02:56:07 -0400

    refactoring phonepicker service

--- src/app/components/phonepicker/phonepicker.service.ts
+++ src/app/components/phonepicker/phonepicker.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const COUNTRY_INFO_URL = 'https://restcountries.eu/rest/v2/all?fields=name;alpha2Code;callingCodes';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +11,6 @@ export class PhonepickerService {
   constructor(private _httpClient: HttpClient) { }
 
   getCountries() {
-    return this._httpClient.get('https://restcountries.eu/rest/v2/all?fields=name;alpha2Code;callingCodes');
+    return this._httpClient.get(COUNTRY_INFO_URL);
   }
 }
